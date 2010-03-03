b105a4d1be239a91c589faa16527fadd3479cef9
Rahul M <rahulm@RMX.local>
2010-03-03 20:11:01 +0530

    [FIX] XHTML Template for SJNDS.

---
+++ xhtml/js/main.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+  $('.wrapper').css('opacity', 0);
+  $('.content').css('left', '-1600px');
+  
+  $('.wrapper').animate({
+    opacity:1
+  }, 200, function(){
+    $('.content').animate({
+      left:0
+    }, 400);
+  });
+  
+  
+  // $('.slide').animate({
+  //     opacity:1
+  //   },3000, function(){
+  //     
+  //     $('.slide2').animate({
+  //       left: '10px'
+  //     }, 1000, function() {
+  // 
+  //       $('.slide3').animate({
+  //           left: '10px'
+  //         }, 1500, function() {
+  //           $('.sliding1').animate({
+  //              opacity: 1,
+  //              left: '300px'
+  //            }, 1500, function() {
+  //              $('.sliding2').animate({
+  //                 opacity: 1,
+  //                 left: '300px'
+  //               }, 1500, function() {
+  //                 $('.sliding3').animate({
+  //                    opacity: 1,
+  //                    left: '300px'
+  //                  }, 1500, function() {
+  // 
+  //                  });
+  //               });
+  //            });
+  //         });
+  //     });
+  //   });
+});
+
+
+
+
+
