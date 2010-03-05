4952b2fc1e7335104026f0407f35cbd961314dd6
Rahul M <rahulm@RMX.local>
2010-03-06 02:01:49 +0530

    [FIX] IE6/7 transparency & JS fixes..

--- sjnds-webby/content/js/main-ie.js
+++ sjnds-webby/content/js/main-ie.js
@@ -1,14 +1,16 @@
-$(document).ready(function() {
-  $('.wrapper').css('left', '1600px');
-  $('.content').css('left', '-1600px');
-  
-  $('.wrapper').animate({
-    left:0
-  }, 600, function(){
-    $('.content').animate({
+$(document).ready(function(){
+  if($('.subnav li:first').hasClass('current') && !$('.nav li:last').hasClass('current') &&$('.nav li:first').hasClass('current')){
+    $('.wrapper').css('left', '1600px');
+    $('.content').css('left', '-1600px');
+
+    $('.wrapper').animate({
       left:0
-    }, 400);
-  });
+    }, 600, function(){
+      $('.content').animate({
+        left:0
+      }, 400);
+    });
+  }
 });
 
 // SCREW YOU MSIE & your crappy filters!! -- rahulmax@insightmethods.com
