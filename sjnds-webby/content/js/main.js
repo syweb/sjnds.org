754c3009446833793d8efbf93e525ac2aba2333e
Rahul M <rahulm@RMX.local>
2010-03-08 20:09:03 +0530

    [FIX] Splash page animation + redirect.. :)

--- sjnds-webby/content/js/main.js
+++ sjnds-webby/content/js/main.js
@@ -1,69 +1,80 @@
 $(document).ready(function() {
-if($('.subnav li:first').hasClass('current') && !$('.nav li:last').hasClass('current') &&$('.nav li:first').hasClass('current')){
-  $('.wrapper').css('opacity', 0);
-  $('.content').css('opacity', '0');
-  $('.subnav').css('left', '-800px');
-  $('.subnav li').css('left', '-800px');
-  $('.content p').css('left', '800px');
-  $('.content h4').css('left', '-800px');
-  $('.content h4').css('position', 'relative');
-  // $('.nav li a').css('opacity', '0');
-  
-  $('.wrapper').animate({
-    opacity:1
-  }, 800, function(){
-    $('.content').animate({
-      opacity:1
-    }, 400, function(){
-      $('.subnav').animate({
-        left:0
-      }, 400);
-      $('.subnav li').animate({
-        left:0
-      }, 300);
-      $('.content p').animate({
-        left:0
-      }, 300);
-      $('.content h4').animate({
-        left:0
-      }, 300);
-    });
-  });
-}
+//Homepage animation
 
-  
+  if($('.subnav li:first').hasClass('current') && !$('.nav li:last').hasClass('current') &&$('.nav li:first').hasClass('current')){
 
-  $('.slide').animate({
+    $('.wrapper').css('opacity', 0);
+    $('.content').css('opacity', '0');
+    $('.subnav').css('left', '-800px');
+    $('.subnav li').css('left', '-800px');
+    $('.content p').css('left', '800px');
+    $('.content h4').css('left', '-800px');
+    $('.content h4').css('position', 'relative');
+  
+    $('.wrapper').animate({
       opacity:1
-    },3000, function(){
-      
-      $('.slide2').animate({
-        left: '10px'
-      }, 1000, function() {
-  
-        $('.slide3').animate({
-            left: '10px'
-          }, 1500, function() {
-            $('.sliding1').animate({
-               opacity: 1,
-               left: '300px'
-             }, 1500, function() {
-               $('.sliding2').animate({
-                  opacity: 1,
-                  left: '300px'
-                }, 1500, function() {
-                  $('.sliding3').animate({
-                     opacity: 1,
-                     left: '300px'
-                   }, 1500, function() {
-  
-                   });
-                });
-             });
-          });
+    }, 800, function(){
+      $('.content').animate({
+        opacity:1
+      }, 400, function(){
+        $('.subnav').animate({
+          left:0
+        }, 400);
+        $('.subnav li').animate({
+          left:0
+        }, 300);
+        $('.content p').animate({
+          left:0
+        }, 300);
+        $('.content h4').animate({
+          left:0
+        }, 300);
       });
     });
+}
 
+// Splash Animation
+  $('.slide').css({'height' : '50px', 'width' : '50px', 'opacity' : '0.4', 'z-index' : '400'});
+  $('.slide2').css({ 'left' : '245px'});
+  $('.slide3').css({'left': '490px'});
+  
+  $('.slide').animate({
+    width: 235,
+    height:175,
+    opacity:1
+  }, 2500);
+
+  $('.slide2').animate({
+    top:0,
+    left:0,
+    opacity:0
+  },600, function(){
+    $('.slide3').animate({
+      top:0,
+      left:0,
+      opacity:0
+    },600, function(){
+      $('.sliding1').animate({
+        left:265,
+        top:10,
+        opacity:1
+      }, 500, function(){
+        $('.sliding2').animate({
+          left:265,
+          top:70,
+          opacity:1
+        }, 500, function(){
+          $('.sliding3').animate({
+            left:265,
+            top:130,
+            opacity:1
+          },500, function(){
+            window.location = '/';
+          });
+        });
+      });
+    });
+  });
 });
 
 
